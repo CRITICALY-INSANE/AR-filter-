44442a3a39b2f8b4b12bfb22d3d0f5c45e95b76f
Sukdeb Ray <37327558+CRITICALY-INSANE@users.noreply.github.com>
2026-06-14 17:57:31 +0530

    Update MainActivity.java

--- src/main/java/com/example/parkourarcore/MainActivity.java
+++ src/main/java/com/example/parkourarcore/MainActivity.java
@@ -8,7 +8,7 @@ import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.pm.;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Environment;
